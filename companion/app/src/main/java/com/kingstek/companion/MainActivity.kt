b5b0d76be2ea8a8e161fa6e84e91ac105fc46e74
KingsleyIke <kingsdanike@gmail.com>
2021-12-13 01:03:29 +0100

    add navigation graph

--- companion/app/src/main/java/com/kingstek/companion/MainActivity.kt
+++ companion/app/src/main/java/com/kingstek/companion/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kingstek.companion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val appBarConfiguration = AppBarConfiguration(navController.graph, drawer_layout)
+
+
     }
 }
