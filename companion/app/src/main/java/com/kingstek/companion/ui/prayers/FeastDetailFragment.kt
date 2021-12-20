b2842c312383e5590356ed0111f28699c3f59136
KingsleyIke <kingsdanike@gmail.com>
2021-12-20 14:21:35 +0100

    update

--- companion/app/src/main/java/com/kingstek/companion/ui/prayers/FeastDetailFragment.kt
+++ companion/app/src/main/java/com/kingstek/companion/ui/prayers/FeastDetailFragment.kt
@@ -1,10 +1,11 @@
-package com.kingstek.companion
+package com.kingstek.companion.ui.prayers
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kingstek.companion.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
